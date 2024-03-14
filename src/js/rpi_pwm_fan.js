f463872c29982e421abf01db1d8b51b9fb6e9054
Prem <premrajreddy16@gmail.com>
2024-03-14 03:18:52 +0000

    Included pwm fan control js code

---
+++ src/js/rpi_pwm_fan.js
@@ -0,0 +1,23 @@
+var client = new VAL_WEB.VALClient("https://localhost:8888");
+
+var entry = new VAL.SubscribeEntry();
+console.log('Adding subscribe entry for fan speed from custom js')
+entry.setPath("Vehicle.Cabin.HVAC.Station.Row1.Driver.FanSpeed");
+entry.setView(TYPES.View.CURRENT_VALUE);
+entry.addFields(TYPES.Field.FIELD_PATH);
+entry.addFields(TYPES.Field.FIELD_VALUE);
+
+var request = new VAL.SubscribeRequest();
+request.addEntries(entry);
+
+var subscribe_stream = client.subscribe(request, metadata);
+subscribe_stream.on('data', function(response) {
+    var updates = response.getUpdatesList();
+    for (var i in updates) {
+        var update = updates[i];
+        var entry = update.getEntry();
+        var dp = entry.getValue();
+        console.log("The data is " +dp);
+        console.log("The type of dp is " + typeof dp);
+    }
+});
