ccf539e351c30410b368a46b6dcc0b658140257c
Prem <premrajreddy16@gmail.com>
2024-03-18 17:12:53 +0000

    Included agl-sense-hat-led-v2 in package.json and temperature.js

--- src/js/temperature.js
+++ src/js/temperature.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const sense = require('trbll-sense-hat-led');
+const sense = require('agl-sense-hat-led-v2');
 const convert = require('color-convert');
 
 // Code for LED present in sensehat as a class
